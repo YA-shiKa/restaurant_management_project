26e1167e3a216f37a5d73ab67a16638038cd9789
YA-shiKa <146869385+YA-shiKa@users.noreply.github.com>
2025-08-04 18:28:07 +0530

    20334

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import CustomUser
+from products.models import Menu
+class Order(models.Model):
+    customer=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    items=models.ManyToManyField(Menu,related_name='orders')
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    order_status=models.CharField(max_length=50,choices=[('Pending','Pending'),('Completed'),('Completed')])
 
-# Create your models here.
+    def __str__(self):
+        return f"Order {self.id}-{self.customer.username} ({self.order_status})"
