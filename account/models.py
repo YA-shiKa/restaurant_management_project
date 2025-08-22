23ab66cf0e4bee2f1a202d6a342648103db30ce0
YA-shiKa <146869385+YA-shiKa@users.noreply.github.com>
2025-08-22 18:36:57 +0530

    20337

--- account/models.py
+++ account/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-class CustomUser(AbstractUser):
-    phone_number=models.CharField(max_length=15,blank=True,null=True, help_text="users phone number")
+class Menu(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.DecimalField(max_digit=5,decimal_place=2)
     def __str__(self):
-        return self.username
+        return self.name
+
+class Order(models.Model):
+    menu_item=models.ForeignKey(Menu,related_name='orders',on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    order_date=models.DateTimeField(auto_now_add=True)
+    total_price=models.DecimalField(max_digit=6,decimal_place=2)
+
+    def save(self,*args,**kwargs):
+        self.total_price=self.menu_item.price*self.quantity
+        super().save(*args,**kwargs)
+    def __str__(self):
+        return f"Order for {self.menu_item.name} (Quantity: {self.quantity})"
