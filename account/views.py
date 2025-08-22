87ff6c211c47a06bd8bf1014ad1da787c42b96ff
YA-shiKa <146869385+YA-shiKa@users.noreply.github.com>
2025-08-22 18:26:57 +0530

    20338

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import response
+from .serializers import DishSerializer
+class MenuView(APIView):
+    def get(self,request):
+        menu=[
+            {"name:"Pasta","description":Creamy Alfredo Pasta","price":12.99}.
+            {"name":"Pizza","description":"Cheese and tomato pizza","price":30}
+        ]
+        serializer=DishSerializer(menu,many=True)
+        return Response(serializer.data)
