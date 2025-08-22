5d7e4984f272cd6c95dbe1fa0711144a29430fc1
YA-shiKa <146869385+YA-shiKa@users.noreply.github.com>
2025-08-22 18:40:25 +0530

    20337

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
-admin.site.register(CustomUser)
-# Register your models here.
+from .models import Menu,Order
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('menu','description','price')
+    search_fields=('name',)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('menu_item','quantity','order_date','total_price')
+    list_filter=('order_date',)
+    search_fields=('menu_item__name')
